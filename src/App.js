fd106b7ae62fece7d4b0a6f3fe581fdd277f0aab
Eddah Karambu <85109484+Eddahkarambu@users.noreply.github.com>
2021-12-09 16:46:49 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import {ChatEngene} from 'react-chat-engine';
+
+const App =()=> {
+  return (
+    <ChatEngene
+    height = '100vh'
+    projectID ='7670f816-c7f5-44af-b9db-1006ab7ee1b4'
+    userName = ''
+    userSecret =''
+
+    
+    />
+   
+  )
+}
+export default App;
